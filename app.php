61e91811255fb991dee69f0efac8a5cc706b5394
Bruno G Alcantara <inovox@gmail.com>
2020-06-21 11:44:03 -0300

    adicionando WikipediaEngine no app.php

--- app.php
+++ app.php
@@ -4,4 +4,7 @@ require 'vendor/autoload.php';
 
 $console = new \Symfony\Component\Console\Application();
 $console->add(new \App\GreetCommand());
+
+$console->add(new \App\Engine\Wikipedia\WikipediaEngine(new \App\Engine\Wikipedia\WikipediaParser(),HttpClient::create()));
+
 $console->run();
